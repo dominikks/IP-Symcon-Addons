32e7ef3ebf1bfe809583ac218ed9f2f2623cda14
Dominik Kus <dominik@kdtk.de>
2022-08-10 20:09:41 +0200

    initial version of HomeMatic Jalousie

---
+++ HomeMaticJalousie/module.php
@@ -0,0 +1,36 @@
+<?php
+
+class HomeMaticJalousie extends IPSModule {
+	// IPS functions
+	public function Create() {
+		parent::Create();
+
+		// Properties erstellen
+		$this->RegisterPropertyInteger("HMUpInstance", 0);
+		$this->RegisterPropertyInteger("HMDownInstance", 0);
+
+		// Variablen erstellen
+		$this->RegisterVariableBoolean("StatusVariable", "Status", "Jalousie");
+		$this->EnableAction("StatusVariable");
+	}
+
+	public function RequestAction($Ident, $Value) {
+		switch($Ident) {
+			case "StatusVariable":
+				$this->SendCommand($Value);
+				break;
+			default:
+				throw new Exception("Invalid Ident");
+		}
+	}
+
+	// Function for the public API within IPS
+	public function SendCommand(bool $Up) {
+		$hminst = $this->ReadPropertyInteger($Up ? "HMUpInstance" : "HMDownInstance");
+		if($hminst) {
+			HM_WriteValueFloat($hminst, "ON_TIME", 0.5);
+			HM_WriteValueBoolean($hminst, "STATE", true);
+		}
+		SetValue($this->GetIDForIdent("StatusVariable"), $Up);
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# IP Symcon Addons
+
+My addon modules for [IP-Symcon](https://www.symcon.de).
+
+## HomeMatic Jalousie
+
+Controls blinds where the up and down button are simply wired to a HomeMatic digital output.
